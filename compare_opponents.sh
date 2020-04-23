845bce2920e182f580a6cc651d2a620c9eec7908
Jack Maloney <jmmaloney4@gmail.com>
2020-04-22 22:32:57 -0500

    Update compare_opponents.sh

--- compare_opponents.sh
+++ compare_opponents.sh
@@ -10,26 +10,38 @@ time swift run TTR ./east.json 25 3000 rrrrr 1.0 >> rvr.out
 time swift run TTR ./east.json 25 3000 rrrrrr 1.0 >> rvr.out
 time swift run TTR ./east.json 25 3000 rrrrrrr 1.0 >> rvr.out
 
+time swift run TTR ./east.json 25 300 brrm $ITERS 0.5 >> exp.out
+time swift run TTR ./east.json 25 300 brrm $ITERS 0.8 >> exp.out
+time swift run TTR ./east.json 25 300 brrm $ITERS 0.9 >> exp.out
+time swift run TTR ./east.json 25 300 brrm $ITERS 1.0 >> exp.out
+time swift run TTR ./east.json 25 300 brrm $ITERS 1.1 >> exp.out
+time swift run TTR ./east.json 25 300 brrm $ITERS 1.2 >> exp.out
+time swift run TTR ./east.json 25 300 brrm $ITERS 1.5 >> exp.out
+time swift run TTR ./east.json 25 300 brrm $ITERS 2.0 >> exp.out
+
 elif [ $1 == "2" ]; then
-time swift run TTR ./east.json 25 3000 bb 1.0 >> rvr.out
-time swift run TTR ./east.json 25 3000 bbb 1.0 >> rvr.out
-time swift run TTR ./east.json 25 3000 bbbb 1.0 >> rvr.out
-time swift run TTR ./east.json 25 3000 bbbbb 1.0 >> rvr.out
-time swift run TTR ./east.json 25 3000 bbbbbb 1.0 >> rvr.out
-time swift run TTR ./east.json 25 3000 bbbbbbb 1.0 >> rvr.out
+time swift run TTR ./east.json 25 3000 bb 1.0 >> bvb.out
+time swift run TTR ./east.json 25 3000 bbb 1.0 >> bvb.out
+time swift run TTR ./east.json 25 3000 bbbb 1.0 >> bvb.out
+time swift run TTR ./east.json 25 3000 bbbbb 1.0 >> bvb.out
+time swift run TTR ./east.json 25 3000 bbbbbb 1.0 >> bvb.out
+time swift run TTR ./east.json 25 3000 bbbbbbb 1.0 >> bvb.out
+
+time swift run TTR ./east.json 25 300 brrm 100000 1.0 >> fun.out
+time swift run TTR ./east.json 25 300 brrm 100000 0.8 >> fun.out
 
 elif [ $1 == "3" ]; then
-time swift run TTR ./east.json 25 300 mm 1.0 $ITERS $ITERS >> rvr.out
-time swift run TTR ./east.json 25 300 mmm 1.0 $ITERS $ITERS $ITERS >> rvr.out
-time swift run TTR ./east.json 25 300 mmmm 1.0 $ITERS $ITERS $ITERS $ITERS >> rvr.out
-time swift run TTR ./east.json 25 300 mmmmm 1.0 $ITERS $ITERS $ITERS $ITERS $ITERS >> rvr.out
-time swift run TTR ./east.json 25 300 mmmmmm 1.0 $ITERS $ITERS $ITERS $ITERS $ITERS $ITERS >> rvr.out
-time swift run TTR ./east.json 25 300 mmmmmmm 1.0 $ITERS $ITERS $ITERS $ITERS $ITERS $ITERS $ITERS >> rvr.out
+time swift run TTR ./east.json 25 300 mm 1.0 $ITERS $ITERS >> mvm.out
+time swift run TTR ./east.json 25 300 mmm 1.0 $ITERS $ITERS $ITERS >> mvm.out
+time swift run TTR ./east.json 25 300 mmmm 1.0 $ITERS $ITERS $ITERS $ITERS >> mvm.out
+time swift run TTR ./east.json 25 300 mmmmm 1.0 $ITERS $ITERS $ITERS $ITERS $ITERS >> mvm.out
+time swift run TTR ./east.json 25 300 mmmmmm 1.0 $ITERS $ITERS $ITERS $ITERS $ITERS $ITERS >> mvm.out
+time swift run TTR ./east.json 25 300 mmmmmmm 1.0 $ITERS $ITERS $ITERS $ITERS $ITERS $ITERS $ITERS >> mvm.out
 
 elif [ $1 == "4" ]; then
-time swift run TTR ./east.json 25 3000 mrrr 1.0 $ITERS >> rvr.out
-time swift run TTR ./east.json 25 3000 rmrr 1.0 $ITERS >> rvr.out
-time swift run TTR ./east.json 25 3000 rrmr 1.0 $ITERS >> rvr.out
-time swift run TTR ./east.json 25 3000 rrrm 1.0 $ITERS >> rvr.out
+time swift run TTR ./east.json 25 3000 mrrr 1.0 $ITERS >> mvr.out
+time swift run TTR ./east.json 25 3000 rmrr 1.0 $ITERS >> mvr.out
+time swift run TTR ./east.json 25 3000 rrmr 1.0 $ITERS >> mvr.out
+time swift run TTR ./east.json 25 3000 rrrm 1.0 $ITERS >> mvr.out
 
 fi
