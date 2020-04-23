46b30118d9833d9d54bf3fa7211b6c3d9d14d7bb
Jack Maloney <jmmaloney4@gmail.com>
2020-04-23 13:36:23 -0500

    New RNG?

--- Package.swift
+++ Package.swift
@@ -14,14 +14,15 @@ let package = Package(
         .package(url: "https://github.com/jmmaloney4/Squall.git", .branch("master")),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: "17.0.0"),
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1"))
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
+        .package(url: "https://github.com/jeudesprits/PSWyhash.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MCTS-TicketToRide",
-            dependencies: ["Squall", "SwiftyJSON", "PathKit", "ArgumentParser"]
+            dependencies: ["Squall", "SwiftyJSON", "PathKit", "ArgumentParser", "PSWyhash"]
         ),
         .testTarget(
             name: "MCTS-TicketToRideTests",
--- Sources/MCTS-TicketToRide/PRNG.swift
+++ Sources/MCTS-TicketToRide/PRNG.swift
@@ -7,15 +7,18 @@
 
 import Foundation
 import Squall
+import PSWyhash
 
-typealias RNG = SystemRandomNumberGenerator
+typealias RNG = WyRandomGenerator
 
 private var RNG_OFFSET: AtomicInt = AtomicInt(0)
 
 func makeRNG() -> RNG {
     // return Gust(offset: UInt32(RNG_OFFSET.incrementAndGet()))
-    return SystemRandomNumberGenerator()
+    // return SystemRandomNumberGenerator()
     // return PRNG(RNG_OFFSET.incrementAndGet())
+    // return WyRand(seed: UInt64(RNG_OFFSET.incrementAndGet()) + UInt64(Date().timeIntervalSince1970.rounded()))
+    return WyRandomGenerator(seed: UInt64(RNG_OFFSET.incrementAndGet()) + UInt64(Date().timeIntervalSince1970.rounded()))
 }
 
 private let P: UInt64 = 514229;
@@ -35,7 +38,7 @@ func gcd(_ A: UInt64, B: UInt64) -> UInt64 {
     return a;
 }
 
-class PRNG: RandomNumberGenerator {
+struct PRNG: RandomNumberGenerator {
     var prev: UInt64
     init(_ offset: Int) {
         var seed = UInt64((Date().timeIntervalSinceReferenceDate)) + UInt64(offset)
@@ -45,20 +48,20 @@ class PRNG: RandomNumberGenerator {
         self.prev = seed
     }
     
-    public func next() -> UInt64 {
+    public mutating func next() -> UInt64 {
         var x = self.prev
         x = x.multipliedReportingOverflow(by: x).partialValue
         x = x.remainderReportingOverflow(dividingBy: M).partialValue
         self.prev = x
-        // print(self.prev)
+        print(self.prev)
         return self.prev
     }
     
-    public func next<T>() -> T where T : FixedWidthInteger, T : UnsignedInteger {
+    public mutating func next<T>() -> T where T : FixedWidthInteger, T : UnsignedInteger {
         return T(self.next())
     }
     
-    public func next<T>(upperBound: T) -> T where T : FixedWidthInteger, T : UnsignedInteger {
+    public mutating func next<T>(upperBound: T) -> T where T : FixedWidthInteger, T : UnsignedInteger {
         return T(self.next()).remainderReportingOverflow(dividingBy: upperBound).partialValue
     }
 }
