5a333ab20cf561ca397e4f8d421067b46a1fc96d
Jack Maloney <jmmaloney4@gmail.com>
2020-04-22 19:39:24 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,3 +6,7 @@ sudo apt-get install clang
 eval "$(curl -sL https://swiftenv.fuller.li/install.sh)"
 which swift
 swift --version
+
+echo 'export SWIFTENV_ROOT="$HOME/.swiftenv"' >> ~/.bash_profile
+echo 'export PATH="$SWIFTENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(swiftenv init -)"' >> ~/.bash_profile
